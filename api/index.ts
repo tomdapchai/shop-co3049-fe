3e5ba95d5d89ed22dc1bd12e35fd84b587d555c7
tomdapchai <vothanhtam2407@gmail.com>
2025-02-17 22:46:11 +0700

    somehow

--- api/index.ts
+++ api/index.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: process.env.NEXT_PUBLIC_BACKEND_URL || "http://localhost:5050",
+    baseURL: process.env.NEXT_PUBLIC_BACKEND_URL,
     withCredentials: true, // Important for CORS with credentials
 });
 
